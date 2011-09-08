8be7fb5d43cf85a6a108a586f11a2e47df48e515
Joe Fredette <jfredett@gmail.com>
2011-09-08 07:29:55 -0400

    add equality checking test for bots

--- spec/bot_spec.rb
+++ spec/bot_spec.rb
@@ -12,4 +12,11 @@ describe Bot do
     bot2 = Bot.new
     bot1.name.should_not == bot2.name
   end
+
+  it "should be able to compare itself to another bot, and tell me if they're the same" do
+    bot0 = Bot.new
+    bot1 = Bot.new
+    bot0.should == bot0
+    bot1.should_not == bot0
+  end
 end
