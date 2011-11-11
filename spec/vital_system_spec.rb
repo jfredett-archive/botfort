c1c96dff1de69a8d9c8b355ec97b2c6102d7d64c
Joe Fredette <jfredett@gmail.com>
2011-11-10 19:10:45 -0500

    refactor and shrinkify vital system spec

--- spec/vital_system_spec.rb
+++ spec/vital_system_spec.rb
@@ -1,32 +1,33 @@
-
 describe VitalSystem do
-  before :each do 
-    @vitals = VitalSystem.new
-  end
+  it { should respond_to :health }
+  it { should respond_to :hurt_for } 
+  it { should respond_to :alive? }
+  it { should respond_to :dead? }
+  it { should be_alive }
+  it { should_not be_dead }
 
   it "should be able to track it's health" do 
-    @vitals.health.should_not be_nil
+    subject.health.should_not be_nil
   end
 
   it "should be able to lose health" do
-    orig_health = @vitals.health
-    @vitals.hurt_for(1).health.should be < orig_health
+    orig_health = subject.health
+    subject.hurt_for(1).health.should be < orig_health
   end
 
   it "should let me set it's health" do
-    @vitals.health = 100
-    @vitals.health.should == 100
-    @vitals.health = 1000
-    @vitals.health.should == 1000
+    subject.health = 100
+    subject.health.should == 100
+    subject.health = 1000
+    subject.health.should == 1000
   end
 
   it "should be able to tell me if it's dead" do
-    @vitals.hurt_for(@vitals.health * 2).should be_dead
+    subject.hurt_for(subject.health * 2).should be_dead
   end
 
   it "should be alive by default" do
-    @vitals.should be_alive
-    @vitals.should_not be_dead
+    subject.should be_alive
+    subject.should_not be_dead
   end
-
 end
