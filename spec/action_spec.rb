380ec0275583eded85d38a88335e960bccac57d9
Joe Fredette <jfredett@gmail.com>
2012-02-24 08:30:46 -0500

    Action.find returns nil if the action is undefined

--- spec/action_spec.rb
+++ spec/action_spec.rb
@@ -12,5 +12,9 @@ describe Action do
     it "should register the action by name" do
       Action.find(:valid_action).should_not be_nil   
     end
+
+    it "should return nil if you search for an action which is undefined" do
+      Action.find(:undefined_action).should be_nil 
+    end
   end
 end
