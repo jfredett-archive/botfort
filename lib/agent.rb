36f543cfdc172f992624a66ab6bf4ce7c64f07b9
Joe Fredette <jfredett@gmail.com>
2012-03-01 08:51:16 -0500

    implement basic claim/perform logic.
    Still missing a lot of functionality -- like executing actions for real.

--- lib/agent.rb
+++ lib/agent.rb
@@ -5,22 +5,44 @@ module Agent
   end
 
   module InstanceMethods
-    def understands?
+    
 
+    def understands?(action_name)
+      __understood_actions.has_key?(action_name)
     end
 
-    def perform
-
+    def perform(action_name)
+      raise ActionNotUnderstood(action_name) unless understands? action_name
+      execute(Action.find(action_name))
     end
 
-    def claim
+    def claim(action_name)
+      __understood_actions[action_name] = Action.find(action_name)
+    end
 
+    def execute(action)
+    end
+
+    private
+
+    def __understood_actions 
+      @__understood_actions ||= {}
     end
   end
 
   module ClassMethods
     def claim 
     end
+
+    def understands?
+
+    end
   end
 
+end
+
+class ActionNotUnderstood < Exception ; end
+
+def ActionNotUnderstood(msg)
+  ActionNotUnderstood.new(msg)
 end
--- spec/agent_spec.rb
+++ spec/agent_spec.rb
@@ -4,22 +4,92 @@ describe Agent do
   it { should be_a Module }
 end
 
-describe "An Agent" do
-  subject do
+describe do
+  before :all do
     class AnAgent
       include Agent
     end
-
-    AnAgent.new
   end
+
   after :all do
     #clean up the namespace
     Object.send(:remove_const, :AnAgent)
   end
 
-  it { should respond_to :perform }
-  it { should respond_to :understands? }
-  it { should respond_to :claim }          # an instance can claim a particular action
-  its(:class) { should respond_to :claim } # by default, an entire class can claim an action
+  subject { AnAgent.new }
+
+  context "basic API" do
+    it { should respond_to :perform }
+    it { should respond_to :understands? }
+    it { should respond_to :claim }                 # an instance can claim a particular action
+    its(:class) { should respond_to :claim }        # by default, an entire class can claim an action
+    its(:class) { should respond_to :understands? } # similarly, an entire class can understand an action
+  end
+
+  context do
+    let!(:valid_action) {
+      action :valid_action do
+        move 0, 1
+        puts "I Moved!"
+      end
+    }
+
+    let!(:valid_action_no_default) { 
+      action :valid_action_no_default
+    }
+
+    context "Agent" do
+      subject { AnAgent }
+
+      describe ".claim" do
+        it "should be able to claim an action with a provided implementation" 
+        it "should be able to claim an action with no default, if it provides one"
+        it "should be able to override a default implementation"
+      end
+
+      describe ".understands?" do
+        subject { AnAgent }
+        it "should understand any action that it claims"
+        it "should not understand any action it does not claim"
+      end
+    end
+
+    context "Agent" do
+      describe "#claim" do
+        it "should be able to claim an action with a provided implementation" 
+        it "should be able to claim an action with no default, if it provides one" 
+        
+        it "should understand any actions it claims to understand" do
+          subject.claim(:valid_action)
+          subject.understands?(:valid_action).should be_true
+        end
+
+        it "should not understand any actions it does not claim to understand" do
+          subject.understands?(:valid_action).should be_false
+        end
+
+        it "should not be able to claim an action unless the dependencies of that action are also understood"
+      end
+
+      describe "#understands?" do
+        it "should be that a new instance understands all of the commands it's class claims"
+        it "should understand any action that it claims"
+        it "should not understand any action it does not claim"
+      end
+
+      describe "#perform" do
+        it "should cause the associated action definition to be executed in the instance context" do
+          subject.claim(:valid_action)
+          subject.should_receive(:execute).with(instance_of(Action))
+          subject.perform(:valid_action)
+        end
+
+        it "should throw an error if the action is not understood by the agent" do
+          #note: subject does not claim to understand valid action
+          expect { subject.perform(:valid_action) }.to raise_error ActionNotUnderstood
+        end
+      end
+    end
+  end
 
 end
